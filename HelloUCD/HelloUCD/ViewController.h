b2679ed106b10b9acd1df79811fa9d8b2c75880a
louiseglynn <cyberluna@gmail.com>
2014-01-08 20:43:01 +0000

    Basic HelloUCD Added

---
+++ HelloUCD/HelloUCD/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HelloUCD
+//
+//  Created by CSI Student on 08/01/2014.
+//  Copyright (c) 2014 ucd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ HelloUCD/HelloUCD/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  HelloUCD
+//
+//  Created by CSI Student on 08/01/2014.
+//  Copyright (c) 2014 ucd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
