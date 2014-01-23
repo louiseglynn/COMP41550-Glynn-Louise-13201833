52080543f10ac71e22bc0cd6c59ea3193c24fbd7
louiseglynn <cyberluna@gmail.com>
2014-01-23 21:07:44 +0000

    Constraints on HelloUCD

---
+++ Constraints/Constraints/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Constraints
+//
+//  Created by CSI Student on 23/01/2014.
+//  Copyright (c) 2014 ucd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Constraints/Constraints/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Constraints
+//
+//  Created by CSI Student on 23/01/2014.
+//  Copyright (c) 2014 ucd. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
